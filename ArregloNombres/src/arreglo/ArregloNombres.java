929446a5785fd5ebb10f5de854dd4cfbac39210e
BSPERDOM <brayan_said1996@hotmail.com>
2025-03-06 21:11:02 -0600

    Tarea 2 Arreglo Nombres

---
+++ ArregloNombres/src/arreglo/ArregloNombres.java
@@ -0,0 +1,23 @@
+package arreglo.nombres;
+
+public class ArregloNombres {
+    public static void main(String[] args) {
+        String[] nombres = {
+            "Juan",
+            "María",
+            "Pedro",
+            "Lucía",
+            "Carlos",
+            "Ana",
+            "Luis",
+            "Elena",
+            "Jorge",
+            "Sofía"
+        };
+        
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
+
